47847d1128c35df9d8d79680dd96bd39368a83f6
trey-k <treykuchinsky@gmail.com>
2021-05-06 17:00:35 -0700

    Added basic tkinter window and Score class

--- coins-overlay/__main__.py
+++ coins-overlay/__main__.py
@@ -1,21 +1,41 @@
 import sys
 import keyboard
+import tkinter as tk
 
+
+class Score:
+    scores = []
+
+    def __init__(self):
+        self.total = tk.IntVar()
+        self.scores.append(self)
+    
+    def increment(self, value=1):
+        self.total.set(self.total.get() + value)
+
+    def decrement(self, value=1):
+        self.total.set(self.total.get() - abs(value))
+
+
+def parse_keys(event):
+    Score.scores[0].decrement()
+    print(Score.scores[0].total)
+    # normalize_name()
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
     # arg parsing with argparse
+    
+    keyboard.on_press(parse_keys)
 
-    keyboard.add_hotkey(78, print, args=["coin +1"]) # numpad +
-    keyboard.add_hotkey(74, print, args=["coin -1"]) # numpad -
-    keyboard.add_hotkey(53, print, args=["reset"]) # numpad /
-    keyboard.add_hotkey(55, print, args=["confetti"]) # numpad *
+    window = tk.Tk()
+    score = Score()
+    label = tk.Label(textvariable=Score.scores[0].total)
+    label.pack()
 
-    print("Press ESC to stop.")
-    keyboard.wait('esc')
-
+    window.mainloop()
 
 if __name__ == "__main__":
     sys.exit(main())
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name = "coinsprogram",
+    name = "coins-overlay",
     version = "0.1.0",
     author = "Trey Kuchinsky",
     author_email = "treykuchinsky@gmail.com",
-    packages = ['coinsprogram'],
+    packages = ['coins-overlay'],
     entry_points = {
         "console_scripts": [
-            "coinsprogram = coinsprogram.__main__:main"
+            "coins-overlay = coins-overlay.__main__:main"
         ]
     },
 )
